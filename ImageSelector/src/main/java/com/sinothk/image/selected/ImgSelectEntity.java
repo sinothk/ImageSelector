c833329f301c8fc4e1c2e938870b2f66cde5b279
LiangYT <381518188@qq.com>
2019-04-03 11:44:31 +0800

    新增图片选择预览

---
+++ ImageSelector/src/main/java/com/sinothk/image/selected/ImgSelectEntity.java
@@ -0,0 +1,50 @@
+package com.sinothk.image.selected;
+
+/**
+ * <pre>
+ *  创建:  梁玉涛 2019/3/29 on 11:52
+ *  项目:  WuMinAndroid
+ *  描述:
+ *  更新:
+ * <pre>
+ */
+public class ImgSelectEntity {
+
+    private String id;
+    private String bizId;
+
+    private String url;
+    private String filePath;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBizId() {
+        return bizId;
+    }
+
+    public void setBizId(String bizId) {
+        this.bizId = bizId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
